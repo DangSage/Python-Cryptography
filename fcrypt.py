7fa89f9496c74085ed7d8eeb350511ddf94ee5b7
DangSage <47837017+DangSage@users.noreply.github.com>
2023-10-22 18:12:45 +0000

    Added messages & key output for debugging reasons

--- fcrypt.py
+++ fcrypt.py
@@ -1,11 +1,20 @@
-"""This is our main file for the project. It will be used to encrypt and decrypt files."""
+"""This is our encryption file. It will be used to encrypt and decrypt files."""
 
 import argparse
 from os import urandom
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP, AES
 
-# Params
+# Currently this iteration of the program only supports encryption
+# This commit only takes encrypt and plaintext as an argument and will not run if decrypt is specified
+# The program will generate a public and private key for the sender and receiver and output
+# the encrypted message to the output file specified by the user
+# The program will also output the private key of the sender for debugging purposes
+
+# To run this program, use the following command:
+# python fcrypt.py --encrypt <input file> <output file>
+
+# Parse arguments
 parser = argparse.ArgumentParser(description='Encrypt or decrypt files using PGP scheme')
 parser.add_argument('--encrypt', action='store_true', help='Encrypt file')
 parser.add_argument('--decrypt', action='store_true', help='Decrypt file')
@@ -20,7 +29,7 @@ with open(args.input_file, 'rb') as f:
 
 key = urandom(16)
 
-print("Before Encryption / Decryption Process")
+print("Before Encryption / Decryption Process:\n====================")
 print(DATA)
 
 if args.encrypt:
@@ -40,15 +49,18 @@ if args.encrypt:
     rsa_encrypt_object = PKCS1_OAEP.new(receiver_public_key)
     key_ciphertext = rsa_encrypt_object.encrypt(key)
 
+    # Write the keys to output for debugging purposes
+    with open("sender_private_key.pem", 'wb') as f:
+        f.write(sender_private_key.export_key('PEM'))
+        
     # Message Concatenation
     concentrated_message = f"{ciphertext}\n|\n{tag}\n|\n{nonce}\n|\n{key_ciphertext}"
-    print("\nDuring Encryption / Decryption Process\n")
-    print(concentrated_message)
+    print("\nDuring Encryption / Decryption Process:\n===================="+concentrated_message)    # Debugging purposes
 
     # Write encrypted message to output file
     with open(args.output_file, 'wb') as f:
         f.write(concentrated_message.encode())
-    print(f"Encrypted message written to {args.output_file}")
+    print(f"\n> Encryption Successful: message written to {args.output_file}")
 
 # Message De-Concatenation
 # elif args.decrypt:
