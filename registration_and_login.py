9306d4f25b46d26becafa28912ac496e62411e15
DangSage <47837017+DangSage@users.noreply.github.com>
2023-10-30 15:59:58 +0000

    Added some documentation and formatting

--- registration_and_login.py
+++ registration_and_login.py
@@ -1,8 +1,9 @@
+"""this is a simple registration and login system that uses RSA 
+encryption to store passwords securely."""
 import json
 import os
 import getpass
 import base64
-from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
@@ -29,6 +30,7 @@ def generate_key_pair():
     
     return private_pem, public_pem
 
+
 # Encrypt the password using the public key
 def encrypt_password(public_key, password):
     public_key = serialization.load_pem_public_key(public_key)
@@ -41,6 +43,7 @@ def encrypt_password(public_key, password):
         )
     )
     return encrypted_password
+
 
 # Decrypt the password using the private key
 def decrypt_password(private_key, encrypted_password):
@@ -55,8 +58,6 @@ def decrypt_password(private_key, encrypted_password):
     )
     return decrypted_password.decode()
 
-# Save user data to a JSON file
-
 
 # Save user data to a JSON file
 def save_user_data(user_data):
@@ -66,6 +67,7 @@ def save_user_data(user_data):
     
     with open('users.json', 'w') as file:
         json.dump(user_data, file)
+
 
 # Load user data from the JSON file
 def load_user_data():
@@ -81,16 +83,6 @@ def load_user_data():
                 return {}
     return {}
 
-'''def save_user_data(user_data):
-    with open('users.json', 'w') as file:
-        json.dump(user_data, file)
-
-# Load user data from the JSON file
-def load_user_data():
-    if os.path.exists('users.json'):
-        with open('users.json', 'r') as file:
-            return json.load(file)
-    return {}'''
 
 # Register a new user
 def register_user():
@@ -107,32 +99,19 @@ def register_user():
     }
 
     save_user_data(user_data)
-    print("Registration successful!")
+    print("Registration successful!\n")
 
-    '''username = input("Enter username: ")
-    password = getpass.getpass("Enter password: ")
-
-    private_key, public_key = generate_key_pair()
-    encrypted_password = encrypt_password(public_key, password)
-
-    user_data = load_user_data()
-    user_data[username] = {
-        'private_key': private_key.decode(),
-        'password': encrypted_password.decode()'''
-    
-
-    #save_user_data(user_data)
-   # print("Registration successful!")
 
 # Verify user credentials
 def verify_user(username, password):
     user_data = load_user_data()
     if username in user_data:
         private_key = user_data[username]['private_key'].encode()
-        stored_password = user_data[username]['password']#.encode()
+        stored_password = user_data[username]['password']
         decrypted_password = decrypt_password(private_key, stored_password)
         return decrypted_password == password
     return False
+
 
 # Login a user
 def login_user():
@@ -140,17 +119,17 @@ def login_user():
     password = getpass.getpass("Enter password: ")
 
     if verify_user(username, password):
-        print("Login successful!")
+        print("Login successful!\n")
     else:
-        print("Login failed. Invalid username or password.")
+        print("Login failed. Invalid username or password.\n")
+
 
 if __name__ == "__main__":
     while True:
         print("1. Register")
         print("2. Login")
         print("3. Quit")
-        choice = input("Enter your choice (1/2/3): ").strip()
-        
+        choice = input("\nEnter your choice (1/2/3): ").strip()
         if choice == '1':
             register_user()
         elif choice == '2':
@@ -158,4 +137,4 @@ if __name__ == "__main__":
         elif choice == '3':
             break
         else:
-            print("Invalid choice. Please select 1, 2, or 3.")
+            print("Invalid choice. Please select 1, 2, or 3.\n")
