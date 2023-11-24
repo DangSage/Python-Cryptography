1d4ea219f2e4e02c0a3edb766c2a2756b1d6ac45
Dang <3thandang@gmail.com>
2023-11-24 18:54:01 -0500

    switched to headers we use in cryptodomex

--- registration_and_login.py
+++ registration_and_login.py
@@ -4,57 +4,32 @@ import json
 import os
 import getpass
 import base64
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives import hashes
-
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import PKCS1_OAEP
+from Cryptodome.Hash import SHA256
 
 def generate_key_pair():
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-    )
-    private_pem = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.TraditionalOpenSSL,
-        encryption_algorithm=serialization.NoEncryption()
-    )
+    private_key = RSA.generate(2048)
+    public_key = private_key.publickey()
 
-    public_key = private_key.public_key()
-    public_pem = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
+    private_pem = private_key.exportKey()
+    public_pem = public_key.exportKey()
 
     return private_pem, public_pem
 
-
 def encrypt_password(public_key, password):
-    public_key = serialization.load_pem_public_key(public_key)
-    encrypted_password = public_key.encrypt(
-        password.encode(),
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
+    public_key = RSA.importKey(public_key)
+    cipher = PKCS1_OAEP.new(public_key, hashAlgo=SHA256)
+    encrypted_password = cipher.encrypt(password.encode())
+
     return encrypted_password
 
-
 def decrypt_password(private_key, encrypted_password):
-    private_key = serialization.load_pem_private_key(private_key, password=None)
-    decrypted_password = private_key.decrypt(
-        encrypted_password,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    return decrypted_password.decode()
+    private_key = RSA.importKey(private_key)
+    cipher = PKCS1_OAEP.new(private_key, hashAlgo=SHA256)
+    decrypted_password = cipher.decrypt(encrypted_password)
 
+    return decrypted_password.decode()
 
 def send_friend_request(sender_email, receiver_email):
     user_data = load_user_data()
@@ -93,6 +68,7 @@ def register_user():
     user_data = load_user_data()
     user_data[email] = {
         'private_key': private_key.decode(),
+        'username': name,
         'password': encrypted_password,
         'friends': [],
         'friend_requests': []  
